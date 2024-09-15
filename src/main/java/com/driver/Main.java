43cd609b8b4a4bbb797ee2bb687cc64991a0749f
Your Name <you@example.com>
2024-09-15 17:02:32 +0530

    doing

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,9 +16,9 @@ public class Main {
 
     public static void main(String[] args) {
         B b = new B();
-        System.out.println(b.meth());  // Print the output of the method call
+        b.meth();  // Print the output of the method call
 
         B b1 = new B();
-        System.out.println(b1.meth());  // Print the output of the second method call
+        b1.meth();  // Print the output of the second method call
     }
 }
